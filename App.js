ac0e2cab8c2d593d06acb44c36659f64ede1e2b8
tomaslf <tomasl.ferreyra@hotmail.com>
2023-01-03 14:45:07 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.firstView}>
+        <Text style={styles.textColor}>Hola Coder!!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.secondView}>
+        <Text style={styles.textColor}>Proximamente Nueva App</Text>
+      </View>
+      <View style={styles.thirdView}>
+        <Image style={styles.img} source={{ uri: "https://res.cloudinary.com/hdsqazxtw/image/upload/v1600707758/coderhouse-logo.png" }} />
+      </View>
     </View>
   );
 }
@@ -13,8 +21,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2C5B5A',
+
+    justifyContent: 'center',
+  },
+  textColor: {
+    color: 'white',
+    fontWeight: '500',
+  },
+  firstView: {
+    flex: 2,
+    backgroundColor: '#5B352C',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  secondView: {
+    flex: 2,
+    backgroundColor: '#3F5B2C',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  thirdView: {
+    flex: 2,
+    backgroundColor: '#5B352C'
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+
+  }
 });
